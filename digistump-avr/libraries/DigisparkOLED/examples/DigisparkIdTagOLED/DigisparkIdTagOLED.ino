8549c6e305b6dfd3bcd860f31fa11ada784b5124
Nic Roche <nicroche@hotmail.com>
2018-04-10 08:20:52 +1000

    Add Name array, refactor

--- digistump-avr/libraries/DigisparkOLED/examples/DigisparkIdTagOLED/DigisparkIdTagOLED.ino
+++ digistump-avr/libraries/DigisparkOLED/examples/DigisparkIdTagOLED/DigisparkIdTagOLED.ino
@@ -4,79 +4,69 @@
 
 #include "bitmaps.h"
 
+const char Mandarin[] = { "ni hao" };
+const char Spanish[] = { "hola" };
+const char English[] = { "hello" };
+const char Hindi[] = { "namaste" };
+const char Arabic[] = { "marhabaan" };
+const char Portuguese[]  = { "   oi   " };
+const char Bengali[]  = { "salaam" };
+const char Russian[]  = { "zdravstvuyte" };
+const char Japanese[]  = { "konnichiwa" };
+const char Punjabi[]  = { "sata sri akala" };
+
 
 void setup() {
   oled.begin();
 }
 
 void loop() {
+  displayPage1();
+  displayPage2();
+  displayPage3();
+}
+
+void displayPage1(){
   oled.clear(); //all black
-  delay(1000);
+//  delay(1000);
+  oled.setFont(FONT8X16);
+  setDelayText(25, 0, 1000, Mandarin);
+  setDelayText(58, 2, 1000, Spanish);
+  setDelayText(28, 4, 1000, English);
+  setDelayText(45, 6, 1000, Hindi);
+  setDelayText(18, 0, 1000, Arabic);
+  setDelayText(40, 2, 1000, Portuguese);
+  setDelayText(20, 4, 1000, Bengali);
+  setDelayText(30, 6, 1000, Russian);
+  setDelayText(4, 0, 1000, Japanese);
+  setDelayText(0, 2, 1000, Punjabi);  
+}
 
-//  oled.setCursor(0, 0); //top left
-//  oled.setFont(FONT8X16);
-//  oled.print(F("ni hao, hola, hello, namaste, marhabaan, oi, salaam, zdravstvuyte, konnichiwa, sata sri akala")); //wrap strings in F() to save RAM!
-//  delay(3000);
-
-oled.setFont(FONT8X16);
-oled.setCursor(20, 0);
-oled.print(F("ni hao"));
-delay(1000);
-
-oled.setCursor(60, 2);
-oled.print(F("hola"));
-delay(1000);
-
-oled.setCursor(20, 4);
-oled.print(F("hello"));
-delay(1000);
-
-oled.setCursor(50, 6);
-oled.print(F("namaste"));
-delay(1000);
-
-oled.setCursor(20, 0);
-oled.print(F("marhabaan"));
-delay(1000);
-
-
-oled.setCursor(40, 2);
-oled.print(F("   oi   "));
-delay(1000);
-
-oled.setCursor(10, 4);
-oled.print(F("zdravstvuyte"));
-delay(1000);
-
-oled.setCursor(40, 6);
-oled.print(F("konnichiwa"));
-delay(1000);
-
-
-oled.setCursor(0, 0);
-oled.print(F("sata sri akala"));
-delay(1000);
-
-
-
+void displayPage2(){
   oled.clear(); 
-  oled.setCursor(30, 2); 
+  oled.setCursor(7, 2); 
   oled.setFont(FONT6X8);
   oled.print(F("I'M")); 
-  oled.setCursor(30, 3);
-  oled.setFont(FONT8X16);
-  oled.print(F("Nic Roche"));
-  oled.setCursor(50, 7); 
+  oled.bitmap(10, 3, 113, 5, name);
+  oled.setCursor(49., 7); 
   oled.setFont(FONT6X8);
-  oled.print(F("FROM")); 
+  oled.print(F("WITH")); 
   delay(3000);
-  
+}
+
+void displayPage3(){
   oled.clear(); 
   //usage oled.bitmap(START X IN PIXELS, START Y IN ROWS OF 8 PIXELS, END X IN PIXELS, END Y IN ROWS OF 8 PIXELS, IMAGE ARRAY);
   oled.setCursor(10, 7);
   oled.setFont(FONT6X8);
   oled.print(F("WWW.IOT123.COM.AU")); 
   oled.bitmap(0, 2, 128, 5, logo);
-
   delay(3000);
 }
+
+void setDelayText(int left, int top, int ms, const char* text){
+  oled.setCursor(left, top);
+  oled.print(text);
+  delay(ms);
+}
+
